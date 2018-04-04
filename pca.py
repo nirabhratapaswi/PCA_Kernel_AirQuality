ec192fac2e9c4eb5110c0a2038b4afcd70ef6599
Nirabhra Tapaswi <nirabhratapaswi@gmail.com>
2018-04-05 02:56:41 +0530

    Preprocessing done

--- pca.py
+++ pca.py
@@ -1,11 +1,12 @@
 print("Importing libraries...")
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, IncrementalPCA
 from sklearn.model_selection import train_test_split
 import kernel_reg
 from sklearn.kernel_ridge import KernelRidge
 from sklearn import preprocessing
 from sklearn.model_selection import ShuffleSplit
+import shuffle
 
 print("Reading data...")
 data = pd.read_csv("AirQualityUCI_clean.csv")
@@ -16,20 +17,26 @@ for x in pop_array:
 
 for i in range(1, 6):
 	print("Forming PCA...")
-	pca = PCA(n_components=i)
-	X = pca.fit_transform(data)
-	rs = ShuffleSplit(n_splits=3, test_size=.25, random_state=0)
-	rs.get_n_splits(X)
+	# pca = PCA(n_components=i)
+	ipca = IncrementalPCA(n_components=i, batch_size=10)
+	# X = pca.fit_transform(data)
+	X = ipca.fit_transform(data)
 
-	print("Splitting data...")
-	X_train, X_test, y_train, y_test = train_test_split(X, popped_array["T"], test_size=0.2, random_state=42)
-	X_train = preprocessing.scale(X_train)
-	X_test = preprocessing.scale(X_test)
-	y_train = preprocessing.scale(y_train)
-	y_test = preprocessing.scale(y_test)
-	print("Fitting data...")
-	# result = kernel_reg.fit(X_test, X_train, y_train, 0.3, 'gs')
-	# print("Hooray motherfuckers")
-	clf = KernelRidge(alpha=1.0)
-	clf.fit(X_train, y_train)
-	print(clf.score(X_test, y_test))
+	print("Shuffling & splitting data...")
+	shuffled_data = shuffle.shuffle(X, popped_array["T"], 10, 0.33)
+	# X_train, X_test, y_train, y_test = train_test_split(X, popped_array["T"], test_size=0.2, random_state=42)
+	for  j in range(0, len(shuffled_data["X"])):
+		# X_train = preprocessing.scale(X_train)
+		# X_test = preprocessing.scale(X_test)
+		# y_train = preprocessing.scale(y_train)
+		# y_test = preprocessing.scale(y_test)
+		X_train = preprocessing.scale(shuffled_data["X"]["train"][j])
+		X_test = preprocessing.scale(shuffled_data["X"]["test"][j])
+		y_train = preprocessing.scale(shuffled_data["y"]["train"][j])
+		y_test = preprocessing.scale(shuffled_data["y"]["test"][j])
+		print("Fitting data for shuffle: ", j, " ...")
+		# result = kernel_reg.fit(X_test, X_train, y_train, 0.3, 'gs')
+		# print("Hooray motherfuckers")
+		clf = KernelRidge(alpha=1.0)
+		clf.fit(X_train, y_train)
+		print(clf.score(X_test, y_test))
---
+++ shuffle.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import ShuffleSplit
+import pandas as pd
+
+def shuffle(X, y, n_splits=3, test_size=0.25):
+	X = list(X)				# convert ndarray to list
+	y = y.values.tolist()	# convert pandas dataframes to values to list
+	print(len(X), len(y))
+	shuffled_X = {"test": [], "train": []}
+	shuffled_y = {"test": [], "train": []}
+	rs = ShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=0)
+	rs.get_n_splits(X)
+	for train, test in rs.split(X):
+		temp_x = []
+		temp_y = []
+		for x in train: 
+			temp_x.append(X[x])
+			temp_y.append(y[x])
+		shuffled_X["train"].append(temp_x)
+		shuffled_y["train"].append(temp_y)
+		temp_x = []
+		temp_y = []
+		for x in test: 
+			temp_x.append(X[x])
+			temp_y.append(y[x])
+		shuffled_X["test"].append(temp_x)
+		shuffled_y["test"].append(temp_y)
+
+	return {"X": shuffled_X, "y": shuffled_y}

1770540d12f46dd19fb40f6b45b764cef1e76cc3
Srikanth Arugula <srikanth.arugula276@gmail.com>
2018-04-05 18:13:51 +0530

    Average score for each dimension

--- pca.py
+++ pca.py
@@ -28,30 +28,31 @@ scores = []
 dims = []
 
 for i in range(1, 13):
-	# pca = PCA(n_components=i)
+
+	print("\nDIMENSION ",i)
 	ipca = IncrementalPCA(n_components=i, batch_size=100)
-	# X = pca.fit_transform(data)
 	X = ipca.fit_transform(data)
 
 	gScore = 0.0
-	print("Shuffling & splitting data...")
 	shuffled_data = shuffle.shuffle(X, popped_array[regression_y], 10, 0.33)
-	# X_train, X_test, y_train, y_test = train_test_split(X, popped_array["T"], test_size=0.2, random_state=42)
-	for  j in range(0, len(shuffled_data["X"]["train"])):
+	shuffles = len(shuffled_data["X"]["train"])
+	for  j in range(0, shuffles):
+		print("Shuffle :", j)
 		X_train = preprocessing.scale(shuffled_data["X"]["train"][j])
 		X_test = preprocessing.scale(shuffled_data["X"]["test"][j])
 		y_train = preprocessing.scale(shuffled_data["y"]["train"][j])
 		y_test = preprocessing.scale(shuffled_data["y"]["test"][j])
-		print("Fitting data for ", i, "th dimensional PCA -- shuffle: ", j, " for target: ", regression_y, "...")
+
 		knn = KNeighborsRegressor(n_neighbors=i)
 		knn.fit(X_train, y_train)
 		print(knn.score(X_test, y_test))
 		score = knn.score(X_test, y_test)
-		if gScore<score:
-			gScore=score
+		gScore = gScore+score
 
+	gScore = gScore/shuffles
 	dims.append(i)
 	scores.append(gScore)
+	print("->Average score for dim ",i, ": ", gScore)
 	
 
 plt.plot(dims, scores, 'b-')

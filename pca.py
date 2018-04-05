053c6dc7fa3808f0ec6eac7d9549868e33c4591c
Srikanth Arugula <srikanth.arugula276@gmail.com>
2018-04-05 17:41:32 +0530

    Plotting Dimension to Accuracy

--- pca.py
+++ pca.py
@@ -1,4 +1,3 @@
-print("Importing libraries...")
 import pandas as pd
 from sklearn.decomposition import PCA, IncrementalPCA
 from sklearn.model_selection import train_test_split
@@ -18,29 +17,40 @@ popped_array = {}
 for x in pop_array:
 	popped_array[x] = pd.DataFrame(data.pop(x))
 
+
+plt.xlabel("components")
+plt.ylabel("accuracy")
+scores = []
+dims = []
+
 for i in range(1, 11):
-	print("Forming PCA...")
 	# pca = PCA(n_components=i)
 	ipca = IncrementalPCA(n_components=i, batch_size=100)
 	# X = pca.fit_transform(data)
 	X = ipca.fit_transform(data)
 
-	print("Shuffling & splitting data...")
-	shuffled_data = shuffle.shuffle(X, popped_array["RH"], 10, 0.33)
-	# X_train, X_test, y_train, y_test = train_test_split(X, popped_array["T"], test_size=0.2, random_state=42)
-	for  j in range(0, len(shuffled_data["X"])):
-		# X_train = preprocessing.scale(X_train)
-		# X_test = preprocessing.scale(X_test)
-		# y_train = preprocessing.scale(y_train)
-		# y_test = preprocessing.scale(y_test)
+	gScore = 0.0
+	print("\nDIMENSION ", i)
+	shuffled_data = shuffle.shuffle(X, popped_array["AH"], 10, 0.33)
+	for  j in range(0, len(shuffled_data["X"]["train"])):
 		X_train = preprocessing.scale(shuffled_data["X"]["train"][j])
 		X_test = preprocessing.scale(shuffled_data["X"]["test"][j])
 		y_train = preprocessing.scale(shuffled_data["y"]["train"][j])
 		y_test = preprocessing.scale(shuffled_data["y"]["test"][j])
+		
 		print("Fitting data for shuffle: ", j, " ...")
-		# clf = KernelRidge(alpha=1.0)
-		# clf.fit(X_train, y_train)
-		# print(clf.score(X_test, y_test))
-		neigh = KNeighborsRegressor(n_neighbors=i)
-		neigh.fit(X_train, y_train)
-		print(neigh.score(X_test, y_test))
+		knn = KNeighborsRegressor(n_neighbors=i)
+		knn.fit(X_train, y_train)
+		score = knn.score(X_test, y_test)
+		if gScore<score:
+			gScore=score
+
+	dims.append(i)
+	scores.append(gScore)
+	
+	print(knn.score(X_test, y_test))
+
+plt.plot(dims, scores, 'b-')
+plt.ylabel("accuracy")
+plt.xlabel("components")
+plt.show()

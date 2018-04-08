3aaf2f448ae05f36db359e36477073999aff79e9
Nirabhra Tapaswi <nirabhratapaswi@gmail.com>
2018-04-08 13:34:21 +0530

    Gaussian kernel added, plots revised

--- bias_variance.py
+++ bias_variance.py
@@ -4,7 +4,15 @@ def bias(y_test, y_predict_shuffled):
 	# y_noise = np.var(y_test, axis=1)
 	if len(np.shape(y_predict_shuffled)) > 1:
 		y_predict_avg = np.mean(y_predict_shuffled, axis=1)
-		return np.mean(np.power(np.subtract(y_test, y_predict_avg), np.array(2)))
+		reshape_y_list = list()
+		for x in y_predict_avg:
+			reshape_y_list.append([x])
+		reshaped_predicted_y = np.array(reshape_y_list)
+		# print("Shape of y_predict_avg: ", np.shape(y_predict_avg), ", reshaped_predicted_y: ", np.shape(reshaped_predicted_y), ", subtracted: ", np.shape(np.subtract(y_test, reshaped_predicted_y)), ", powered: ", np.shape(np.power(np.subtract(y_test, reshaped_predicted_y), np.array(2))))
+		# print("Shape of y_predict_avg: ", np.shape(y_predict_avg.tolist()), ", y_test: ", np.shape(y_test.tolist()))
+		print("Shape of y_predict_avg: ", np.shape(y_predict_avg.tolist()), ", y_test: ", np.shape(y_test.tolist()), ", subtracted: ", np.shape(np.subtract(y_test.tolist(), y_predict_avg.tolist())), ", powered: ", np.shape(np.power(np.subtract(y_test.tolist(), y_predict_avg.tolist()), np.array(2))))
+		# return np.mean(np.power(np.subtract(y_test, reshaped_predicted_y), np.array(2)))
+		return np.mean(np.power(np.subtract(y_test.tolist(), y_predict_avg.tolist()), np.array(2)))
 	else:
 		y_predict_avg = np.sum(y_predict_shuffled)
 		return np.mean(np.power(np.subtract(y_test, y_predict_avg), np.array(2)))
--- pca_optimize.py
+++ pca_optimize.py
@@ -14,6 +14,7 @@ from sklearn.neighbors import KNeighborsRegressor
 import sys
 import time
 import bias_variance as bv
+import math
 
 regression_y = "T"
 dim_range = {"min": 2, "max": 3}
@@ -44,12 +45,42 @@ def find_nearest(array, value):
 
 	return index
 
+def gaussian_distance(dist, sigma):
+	# print(dist.tolist())
+	dist = dist.tolist()
+	count = 0
+	for x in dist:
+		dist[count] = (1/(math.sqrt(2*math.pi)*sigma))*math.exp(-pow(x/sigma, 2)/2)
+		# if np.isnan(dist[count]):
+		# 	dist[count] = 0
+		# if np.isinf(dist[count]):
+		# 	dist[count] = 0
+		if dist[count] == 0:
+			dist[count] = 100
+		count += 1
+
+	mean_dist = np.mean(np.array(dist))
+	count = 0
+	for x in dist:
+		if dist[count] == 0:
+			dist[count] = mean_dist	
+		count += 1
+	return np.array(dist)
+
 def my_distance(weights):
-    return weights
+	count = 0
+	for x in weights:
+		# sigma = math.sqrt(np.var(np.array(x)))
+		sigma = np.mean(np.array(x))# 0.050
+		weights[count, :] = gaussian_distance(x, sigma)
+		# print(weights[count, :])
+		count += 1
+
+	return weights
 
 scores = []
 dims = []
-neighbor_range = {"start": 2, "end": 50, "gap": 1}
+neighbor_range = {"start": 2, "end": 30, "gap": 2}
 
 for i in range(dim_range["min"], dim_range["max"]):
 
@@ -72,7 +103,8 @@ for i in range(dim_range["min"], dim_range["max"]):
 			X_test = preprocessing.scale(shuffled_data["X"]["test"][j])
 			y_train = preprocessing.scale(shuffled_data["y"]["train"][j])
 			y_test = preprocessing.scale(shuffled_data["y"]["test"][j])
-			knn = KNeighborsRegressor(n_neighbors=n_neighbors, weights='distance', algorithm='auto', leaf_size=30, p=2, metric='minkowski', n_jobs=-1)
+			# knn = KNeighborsRegressor(n_neighbors=n_neighbors, weights='distance', algorithm='auto', leaf_size=30, p=2, metric='minkowski', n_jobs=-1)
+			knn = KNeighborsRegressor(n_neighbors=n_neighbors, weights=my_distance, algorithm='auto', leaf_size=30, p=2, metric='minkowski', n_jobs=-1)
 			knn.fit(X_train, y_train)
 			y_fit = knn.predict(X_test)
 			y_predict.append(y_fit)
@@ -80,19 +112,18 @@ for i in range(dim_range["min"], dim_range["max"]):
 			score = knn.score(X_test, y_test)
 			cumScore = cumScore + score
 
-		print("Shape of y_predict: ", np.shape(y_predict), ", of y_test: ", np.shape(y_test))
+		# print("Shape of y_predict: ", np.shape(y_predict), ", of y_test: ", np.shape(y_test))
 		avgScore = cumScore / shuffles
 		dims.append(i)
 		scores.append(avgScore)
-		print("Variance in y_predict: ", np.mean(np.var(np.array(y_predict), axis=1)), ", Average score: ", avgScore)
 		bias.append(pow(bv.bias(y_test, np.swapaxes(y_predict, 0, 1)), 2))
 		variance.append(bv.variance(y_predict))
-		print("Variance in y_predict: ", variance[int((n_neighbors-neighbor_range["start"])/neighbor_range["gap"])], ", bias: ", bias[int((n_neighbors-neighbor_range["start"])/neighbor_range["gap"])])
+		print("Variance in y_predict: ", variance[int((n_neighbors-neighbor_range["start"])/neighbor_range["gap"])], ", bias: ", bias[int((n_neighbors-neighbor_range["start"])/neighbor_range["gap"])], ", average score: ", avgScore)
 		# acc_var_arr.append(gScore**2/mean_variance)
 		# print("Accuracy^2 / Variance: ", gScore**2/mean_variance)
 
-	plot_1.xlabel("bias/variance")
-	plot_1.ylabel("n_neighbors")
+	plot_1.xlabel("n_neighbors")
+	plot_1.ylabel("Percentage fraction")
 	plot_1.plot(range(neighbor_range["start"], neighbor_range["end"], neighbor_range["gap"]), bias, '-', label="bias")
 	plot_1.plot(range(neighbor_range["start"], neighbor_range["end"], neighbor_range["gap"]), variance, 'r-', label="variance")
 	
@@ -100,4 +131,10 @@ print("Optimum dimension: ", find_nearest(scores, 0.85*max(scores)))
 plot_1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 plot_1.title("For dimension starting from " + str(dim_range["min"]))
 plot_1.show()
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+plt.title("For dimension starting from " + str(dim_range["min"]))
+plot_1.xlabel("n_neighbors")
+plot_1.ylabel("Accuracy")
+plot_1.plot(range(neighbor_range["start"], neighbor_range["end"], neighbor_range["gap"]), scores, '-', label="accuracy")
+plt.show()
 # time.sleep(30)
